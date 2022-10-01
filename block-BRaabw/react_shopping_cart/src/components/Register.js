0050acbf042f99cb8c070faaf366214b0bf930b0
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-10-01 11:59:00 +0530

    add authentication

--- block-BRaabw/react_shopping_cart/src/components/Register.js
+++ block-BRaabw/react_shopping_cart/src/components/Register.js
@@ -2,55 +2,67 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-function Login() {
+function Register() {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [login, setLogin] = useState(false);
+  const [register, setRegister] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const login = async () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const register = async () => {
       try {
         const res = await axios({
           method: 'POST',
-          url: `https://shopping-cart-react-backend.herokuapp.com/api/users/login`,
+          url: `https://shopping-cart-react-backend.herokuapp.com/api/users/register`,
           headers: {
             'Content-Type': 'application/json',
           },
           data: JSON.stringify({
             user: {
+              username,
               email,
               password,
             },
           }),
         });
-        setLogin(true);
-        localStorage.setItem('user', JSON.stringify(res.data.user));
-        window.location.href = '/';
+        setRegister(true);
+        window.location.href = '/login';
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    login();
+    register();
   };
 
   return (
     <>
-      <h1 className="text-4xl text-center mt-8">Sign In</h1>
+      <h1 className="text-4xl text-center mt-8">Sign Up</h1>
       <form className="my-6 w-2/6 mx-auto" onSubmit={(e) => handleSubmit(e)}>
         <input
           type="text"
+          placeholder="Username"
+          className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+       
+        <input
+          type="text"
           placeholder="Email"
-          className="text-lg border block mx-auto px-5 py-2 w-full rounded"
+          className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
           id="email"
           name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        
         <input
           type="password"
           placeholder="Password"
-          className="text-lg border block mx-auto px-5 py-2 w-full rounded"
+          className="text-lg border block mx-auto px-5 py-2 mb-4 w-full rounded"
           id="password"
           name="password"
           value={password}
@@ -62,13 +74,13 @@ function Login() {
         className=" bg-green-500 hover:bg-green-dark text-white text-lg px-6 py-2 rounded block mx-auto"
         onClick={(e) => handleSubmit(e)}
       >
-        Sign in
+        Sign up
       </button>
-      <Link to="/register" className="block text-center text-green-500 mt-8">
-        Create an account
+      <Link to="/login" className="block text-center text-green-500 mt-8">
+        Already have an account? Sign in
       </Link>
     </>
   );
 }
 
-export default Login;
+export default Register;
